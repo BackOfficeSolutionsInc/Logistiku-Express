d767ba30af96168d8308c9c2371afe4dae50e182
ryanespina <43159176+ryanespina@users.noreply.github.com>
2019-03-22 15:18:56 +0800

    Ryan/Update 3222019

---
+++ constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6Le0k5cUAAAAAMrc-ojbZZd94p9BrPF01OIv4TUm"); 
+define('SECRET_KEY',"6Le0k5cUAAAAAGobOdpJMrGqC4Gz25boJQZQ_8tM");
+?>
